4950a3394c865128766765981e2dc4db659c643c
manhhoang8th4 <manhhoang8th4@gmail.com>
2025-02-25 02:01:08 +0700

    Update User

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 // @router Post /api/users/register
@@ -20,18 +20,79 @@ router.post("/register", async (req, res) => {
     user = new User({ name, email, password });
     await user.save();
 
-    res.status(201).json({
-      user: {
-        _id: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      },
-    });
+    //createjwt payload
+    const payload = { user: { id: user._id, role: user.role } };
+    //sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
+});
+//@route post api/users/login
+//@desc authenticate user
+//@access public
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid User" });
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid Password" });
+
+    const payload = { user: { id: user._id, role: user.role } };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //send the user and token in response
+        res.json({
+          user: {
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error    ");
+  }
+});
+
+//@route get /api/users/profile
+//@desc get login user profile
+//@access private
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// connect to mongodb
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO RABBIIT API");
+});
+
+//API routes
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
