1abbdd751db125ec3844a968bf6631c3865c735a
manhhoang8th4 <manhhoang8th4@gmail.com>
2025-02-28 01:32:35 +0700

    Update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,6 +13,7 @@ router.post("/", protect, admin, async (req, res) => {
       discountPrice,
       countInStock,
       category,
+      brand,
       sizes,
       colors,
       collections,
@@ -34,6 +35,7 @@ router.post("/", protect, admin, async (req, res) => {
       discountPrice,
       countInStock,
       category,
+      brand,
       sizes,
       colors,
       collections,
@@ -51,6 +53,83 @@ router.post("/", protect, admin, async (req, res) => {
 
     const createdProduct = await product.save();
     res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//api/products/:id
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isFeatured : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weigth = weight || product.weigth;
+      product.sku = sku || product.sku;
+
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//api/products/delete
+
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.json({ message: "Product removed" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
