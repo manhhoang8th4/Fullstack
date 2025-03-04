e612887479860cb6e8aa625f94546412358e54c2
manhhoang8th4 <manhhoang8th4@gmail.com>
2025-03-04 11:26:12 +0700

    Update

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+    const streamUpload = (fillBuffer) => {
+      return new Promise((relsove, reject) => {
+        const stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+          }
+        });
+      });
+    };
+  } catch (error) {}
+});
